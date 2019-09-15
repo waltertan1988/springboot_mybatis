f1ffd5a2f1494e1bd071bb9988acdf9f35236eef
waltertan <waltertan1988@163.com>
2019-09-15 21:42:11 +0800

    测试MyBatis Generator的增删改

--- src/main/java/com/walter/domain/mbg/MbgEmployee.java
+++ src/main/java/com/walter/domain/mbg/MbgEmployee.java
@@ -1,8 +1,12 @@
 package com.walter.domain.mbg;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MbgEmployee {
     /**
      *
--- src/test/java/com/walter/MgbTests.java
+++ src/test/java/com/walter/MgbTests.java
@@ -1,5 +1,6 @@
 package com.walter;
 
+import com.walter.domain.mbg.MbgEmployee;
 import com.walter.domain.mbg.MbgEmployeeExample;
 import com.walter.mapper.mbg.MbgEmployeeMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.UUID;
 import java.util.function.Consumer;
 
 @RunWith(SpringRunner.class)
@@ -29,16 +31,50 @@ public class MgbTests {
     }
 
     @Test
-    public void testMbg(){
+    public void testMbgSelect(){
         this.handleMbgEmployee(mapper -> {
             // 查询所有
-            mapper.selectByExample(null).forEach(mbgEmployee -> log.info(mbgEmployee.toString()));
+            mapper.selectByExample(null).forEach(mbgEmployee -> log.info("Case1: {}", mbgEmployee.toString()));
 
             // 条件查询
             MbgEmployeeExample mbgEmployeeExample = new MbgEmployeeExample();
             mbgEmployeeExample.createCriteria().andUserRealNameLike("%Walter%").andGenderEqualTo("M"); // AND条件
             mbgEmployeeExample.or(mbgEmployeeExample.createCriteria().andUserRealNameLike("%Cathy%")); // OR条件
-            mapper.selectByExample(mbgEmployeeExample).forEach(mbgEmployee -> log.info(mbgEmployee.toString()));;
+            mapper.selectByExample(mbgEmployeeExample).forEach(mbgEmployee -> log.info("Case2: {}", mbgEmployee.toString()));
+
+            log.info("Case3: {}", mapper.countByExample(mbgEmployeeExample));
+        });
+    }
+
+    @Test
+    public void testMbgInsert(){
+        this.handleMbgEmployee(mapper -> {
+            MbgEmployee mbgEmployee = new MbgEmployee(null, UUID.randomUUID().toString(),"YYY","Z","AAA","D0001");
+            int count = mapper.insertSelective(mbgEmployee);
+            log.info("id/count: {}/{}", mbgEmployee.getId(), count);
+        });
+    }
+
+    @Test
+    public void testMbgUpdateSelective(){
+        this.handleMbgEmployee(mapper -> {
+            MbgEmployee mbgEmployee = new MbgEmployee();
+            mbgEmployee.setUserRealName("new user real name");
+
+            MbgEmployeeExample example = new MbgEmployeeExample();
+            example.createCriteria().andEmailEqualTo("AAA");
+            int count = mapper.updateByExampleSelective(mbgEmployee, example);
+            log.info("count: {}", count);
+        });
+    }
+
+    @Test
+    public void testMbgDelete(){
+        this.handleMbgEmployee(mapper -> {
+            MbgEmployeeExample example = new MbgEmployeeExample();
+            example.createCriteria().andEmailEqualTo("AAA");
+            int count = mapper.deleteByExample(example);
+            log.info("count: {}", count);
         });
     }
 }
